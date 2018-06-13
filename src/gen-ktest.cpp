12c79798316c3e7c08299dbd610559389f5c5fea
Quoc-Sang Phan <dark2bright@gmail.com>
2018-06-13 10:43:00 -0700

    update

--- src/gen-ktest.cpp
+++ src/gen-ktest.cpp
@@ -21,8 +21,9 @@
 #define stat64 stat
 #endif
 
-
 #define MAX 64
+#define MAX_SIZE 30
+
 static void push_int(KTest *b, const char *name, unsigned val) {
   KTestObject *o = &b->objects[b->numObjects++];
   assert(b->numObjects < MAX);
@@ -36,11 +37,34 @@ static void push_int(KTest *b, const char *name, unsigned val) {
          o->bytes[i] = (val << (i * 8));  
 }
 
+/*
+User this option if the symbolic variables are of interger type, created using
+the API klee_make_symbolic
+*/
+static void gen_from_vars(KTest *b, const char *input_file, const char *output_file){
+  char sym_var[MAX_SIZE];
+  unsigned int val;
+  std::ifstream infile(input_file);
+  while (infile >> sym_var >> val)
+  {
+    std::cout << sym_var << " " << val << std::endl;
+    push_int(b, sym_var, val);
+  }
+  
+  if (!kTest_toFile(b, output_file))
+    assert(0);
+}
 
-#define MAX_SIZE 30
+/*
+Use this option if the input file is symbolic
+*/
+static void gen_from_file(KTest *b, const char *input_file, const char *output_file){
+
+}
+
 int main(int argc, char *argv[]) {
-  if (argc < 3) {
-    fprintf(stderr, "Usage: %s <input-file> <output-file>\n", argv[0]);
+  if (argc < 4) {
+    fprintf(stderr, "Usage: %s <option> <input-file> <output-file>\n", argv[0]);
     fprintf(stderr, "each line of <input-file> has the form: variable value\n");
     exit(1);
   }
@@ -54,16 +78,13 @@ int main(int argc, char *argv[]) {
   b.numObjects = 0;
   b.objects = (KTestObject *)malloc(MAX * sizeof *b.objects);
   
-  char sym_var[MAX_SIZE];
-  unsigned int val;
-  std::ifstream infile(argv[1]);
-  while (infile >> sym_var >> val)
-  {
-    std::cout << sym_var << " " << val << std::endl;
-    push_int(&b, sym_var, val);
-  }
-  
-  if (!kTest_toFile(&b, argv[2]))
-    assert(0);
+	if(strcmp(argv[1],"--sym-vars") == 0){
+		gen_from_vars(&b, argv[2], argv[3]);		
+	} else if(strcmp(argv[1], "--sym-file") == 0){
+		gen_from_file();
+	} else{
+    fprintf(stderr, "Unknown option: %s\n", argv[1]);
+	}
+	
   return 0;
 }
