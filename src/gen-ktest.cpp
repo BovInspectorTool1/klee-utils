d0487952ae995049430810c027f6763aef02912c
Quoc-Sang Phan <dark2bright@gmail.com>
2018-06-13 13:02:14 -0700

    generating ktest file for symbolic file

--- src/gen-ktest.cpp
+++ src/gen-ktest.cpp
@@ -24,7 +24,7 @@
 #define MAX 64
 #define MAX_SIZE 30
 
-static void push_int(KTest *b, const char *name, unsigned val) {
+static void push_int(KTest *b, const char *name, int val) {
   KTestObject *o = &b->objects[b->numObjects++];
   assert(b->numObjects < MAX);
   // o->name = strdup(name);
@@ -32,34 +32,77 @@ static void push_int(KTest *b, const char *name, unsigned val) {
   // integer in C++ is 4 bytes
   o->numBytes = 4;
   o->bytes = (unsigned char *)malloc(4);
+	//FIXME: the bytes are not displayed correctly in ktest-tool for value > 40
+	o->bytes[0] = val & 0xFF;
+	std::cout << "First byte is " << (o->bytes[0]) << std::endl;
+	o->bytes[1] = (val >> 8) & 0xFF;
+	std::cout << "Second byte is " << (o->bytes[1]) << std::endl;
+  o->bytes[2] = (val >> 16) & 0xFF;
+	std::cout << "Third byte is " << (o->bytes[2]) << std::endl;
+  o->bytes[3] = (val >> 24) & 0xFF;
+	std::cout << "Fourth byte is " << (o->bytes[3]) << std::endl;
+}
+
+static void push_obj(KTest *b, const char *name, unsigned non_zero_bytes, 
+                     unsigned total_bytes) {
+  KTestObject *o = &b->objects[b->numObjects++];
+  assert(b->numObjects < MAX);
+
+  o->name = strdup(name);
+  o->numBytes = total_bytes;
+  o->bytes = (unsigned char *)malloc(o->numBytes);
+
   unsigned i;
-  for (int i = 0; i < 4; i++)
-         o->bytes[i] = (val << (i * 8));  
+  for(i = 0; i < non_zero_bytes; i++)
+    o->bytes[i] = random();
+
+  for(i = non_zero_bytes; i < total_bytes; i++)
+    o->bytes[i] = 0;
 }
 
 /*
 User this option if the symbolic variables are of interger type, created using
 the API klee_make_symbolic
 */
-static void gen_from_vars(KTest *b, const char *input_file, const char *output_file){
+static void gen_from_vars(KTest *b, const char *input_file){
   char sym_var[MAX_SIZE];
-  unsigned int val;
+  int val;
   std::ifstream infile(input_file);
   while (infile >> sym_var >> val)
   {
     std::cout << sym_var << " " << val << std::endl;
     push_int(b, sym_var, val);
   }
-  
-  if (!kTest_toFile(b, output_file))
-    assert(0);
 }
 
 /*
 Use this option if the input file is symbolic
 */
-static void gen_from_file(KTest *b, const char *input_file, const char *output_file){
+static void gen_from_file(KTest *b, const char *input_file){
+ 	std::ifstream inFile; 
+	size_t size = 0;
+	inFile.open(input_file, std::ios::in|std::ios::binary|std::ios::ate);
+	
+	if(inFile.is_open()) {
 
+		inFile.seekg(0, std::ios::end);
+		size = inFile.tellg();
+		inFile.seekg(0, std::ios::beg);
+
+		KTestObject *o = &b->objects[b->numObjects++];
+	  assert(b->numObjects < MAX);
+	  o->name = strdup("file");
+	  o->numBytes = size + 1;
+	  o->bytes = (unsigned char *)malloc(size + 1);
+
+		// FIXME: is it a good type cast?
+		inFile.read((char*)o->bytes, size);
+
+		// Does it need to be terminated by '\0'
+		o->bytes[size] = '\0';
+		// FIXME: Not sure if this is necessary? but KLEE used in generating random seed
+		push_obj(b, "file-stat", sizeof(struct stat64), sizeof(struct stat64));
+	}
 }
 
 int main(int argc, char *argv[]) {
@@ -79,12 +122,15 @@ int main(int argc, char *argv[]) {
   b.objects = (KTestObject *)malloc(MAX * sizeof *b.objects);
   
 	if(strcmp(argv[1],"--sym-vars") == 0){
-		gen_from_vars(&b, argv[2], argv[3]);		
+		gen_from_vars(&b, argv[2]);		
 	} else if(strcmp(argv[1], "--sym-file") == 0){
-		gen_from_file();
+		gen_from_file(&b, argv[2]);
 	} else{
-    fprintf(stderr, "Unknown option: %s\n", argv[1]);
+    fprintf(stderr, "Unexpected option: <%s>\n", argv[1]);
 	}
-	
+
+  if (!kTest_toFile(&b, argv[3]))
+    assert(0);	
+
   return 0;
 }
